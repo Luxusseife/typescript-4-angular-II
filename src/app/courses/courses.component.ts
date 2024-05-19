d510773af62f11867ad6066895efd2225ae7a14c
Jenny Lind <jeli2308@student.miun.se>
2024-05-19 20:16:15 +0200

    Skapat en filterfunktion som fungerar för kurskod och kursnamn.

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CourseService } from '../services/course.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
@@ -15,6 +16,8 @@ export class CoursesComponent {
   // Egenskaper.
   courseList: Course[] = [];
   image: string = "assets/images/rabbit.png";
+  filteredCourseList: Course[] = [];
+  filterValue: string = "";
 
   // Konstruktor.
   constructor(private courseService: CourseService) { }
@@ -23,6 +26,16 @@ export class CoursesComponent {
   ngOnInit() {
     this.courseService.getCourses().subscribe(data => {
       this.courseList = data;
+      this.filteredCourseList = data;
     })
   }
+
+  // Filtrering av kursnamn och/eller kurskod.
+  applyFilter(): void {
+    this.filteredCourseList = this.courseList.filter((course) =>
+      course.coursename.toLowerCase().includes(this.filterValue.toLowerCase()) ||
+      course.code.toLowerCase().includes(this.filterValue.toLowerCase())
+    );
+  }
+
 }
