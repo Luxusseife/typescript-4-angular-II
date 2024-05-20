245ebc62454514aafd5cde1790c134d7293c7872
Jenny Lind <jeli2308@student.miun.se>
2024-05-20 10:51:48 +0200

    Uppdaterat routes och lagt till _redirects-filen under assets.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,4 @@ import { CoursesComponent } from './courses/courses.component';
 export const routes: Routes = [
     { path: "courses", component: CoursesComponent },
     { path: "", redirectTo: "/courses", pathMatch: "full" },
-    
-    { path: "404", redirectTo: "/courses", pathMatch: "full" },
-    { path: "**", redirectTo: "/courses", pathMatch: "full" }
 ];
